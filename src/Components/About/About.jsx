259c9b4911a2ce6d6f2848baae1306dd74fcb81b
Kosisochukwu Emmanuel <realitynuel@gmail.com>
2024-11-09 13:36:44 +0100

    working on the about section

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -9,13 +9,13 @@ const About = () => {
   return (
     <section className="w-full h-[200vh] flex items-center justify-center border-4 border-black rounded-t-[300px] bg-blue-500">
       <div className="w-[85%] h-5/6 bg-red-500 rounded-t-[150px] ">
-        <div className="md:flex items-center justify-center p-[5rem] gap-[10rem] h-[45%] rounded-t-[150px] border-2 border-black">
-          <div className="w-[50%]  flex items-center justify-center rounded-tl-[100px] p-[1rem] bg-white border-2 border-black">
+        <div className="md:flex items-center justify-center p-[5rem] gap-[7rem] h-[45%] rounded-t-[150px] border-2 border-black">
+          <div className="w-[40%] h-[85%]  flex items-center justify-center rounded-tl-[100px] p-[1rem] bg-white border-2 border-black">
             <img src={AboutImage} alt="about-left-image" />
           </div>
 
-          <div>
-            <h1 className="bg-green-500">About us</h1>
+          <div className="w-[55%] h-full border-2 border-black">
+            <h1 className="bg-green-500 inline-block p-lg" >About us</h1>
             <h2>Built to Empower Your Creative Journey</h2>
             <p>
               Designed to help you share, connect, and grow with the support of
